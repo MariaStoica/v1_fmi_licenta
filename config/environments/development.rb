289772c043f309e573987bd5b4c9fa54d3613f13
Maria Stoica <maria.stoica13@gmail.com>
2014-08-07 23:00:45 +0300

    try 1 fix mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ V1FmiLicenta::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'fmiunivbuc@gmail.com',
+      password:             'fmiunibuc',
+      authentication:       'login',
+      enable_starttls_auto: true  }
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ V1FmiLicenta::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'fmiunivbuc@gmail.com',
+      password:             'fmiunibuc',
+      authentication:       'login',
+      enable_starttls_auto: true  }
+  
 end
